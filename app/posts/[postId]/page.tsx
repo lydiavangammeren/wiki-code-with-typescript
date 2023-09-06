4504f345e6e140d7b2b42f748f01292d7662b9bd
Lydia <67099359+lydiavangammeren@users.noreply.github.com>
2023-09-06 20:41:10 +0200

    added PostPage + PostDetails + styling

---
+++ app/posts/[postId]/page.tsx
@@ -0,0 +1,28 @@
+import PostDetails from "@/components/PostDetails";
+import prisma from "@/lib/prisma";
+import { notFound } from "next/navigation";
+
+interface Props {
+    params: {
+        postId: string;
+    }
+}
+
+export default async function PostPage({params}: Props) {
+    
+    const post = await prisma.post.findUnique({
+        where: {
+            id: Number(params.postId)
+        },
+        include: {
+            author: true
+        }
+    })
+
+    if (!post) {
+        return notFound();
+    }
+    return (
+        <PostDetails {...post}/>
+    )
+}
