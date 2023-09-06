4e17bbeddc0fcae6e3ad26ba05a9fde801078eba
Lydia <67099359+lydiavangammeren@users.noreply.github.com>
2023-09-06 12:06:17 +0200

    initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,36 @@
+"use client"
+import Link from 'next/link'
+import {usePathname} from 'next/navigation'
+import styles from './Navbar.module.css'
+
+export default function Navbar() {
+    const pathname = usePathname();
+    
+    function isActive(linkpathname: string) {
+        return pathname === linkpathname
+    }
+
+return (
+    <nav>
+        <div className={styles.left}>
+        <Link href="/" data-active={isActive(
+        "/"
+        )}>Wiki Code</Link>
+        <Link href="/drafts" data-active={isActive(
+        "/drafts"
+        )}>Drafts</Link>
+        <Link href="/about" data-active={isActive(
+        "/about"
+        )}>About</Link>
+        </div>
+        <div className={styles.right}>
+        <Link href="/signup" data-active={isActive(
+        "/signup"
+        )}>Sign Up</Link>
+        <Link href="/create" data-active={isActive(
+        "/create"
+        )}>+ Create Post</Link>
+        </div>
+    </nav>
+)
+}
