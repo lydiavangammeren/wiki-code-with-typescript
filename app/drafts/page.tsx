bedbade0f0c0158fc04fcaa755aa336d38965f17
Lydia <67099359+lydiavangammeren@users.noreply.github.com>
2023-09-06 20:00:39 +0200

    added createpost page and drafts page

---
+++ app/drafts/page.tsx
@@ -0,0 +1,18 @@
+import prisma from "@/lib/prisma";
+import Post from "@/components/Post";
+import styles from "./Drafts.module.css"
+
+export default async function DraftsPage() {
+  const drafts = await prisma.post.findMany({where: {published: false}, include: {author: true}});
+
+  return (
+    <main>
+      <h1>Drafts</h1>
+      {drafts.map(post => (
+        <div key={post.id} className={styles.draft}>
+          <Post post={post}/>
+        </div>
+      ))}
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 import Post from "@/components/Post";
 import prisma from "@/lib/prisma";
 
-
 export default async function HomePage() {
-  const posts = await prisma.post.findMany({where: {published: true}, include: {author: true}});
+  const posts = await prisma.post.findMany({
+    where: { published: true },
+    include: { author: true },
+  });
   return (
-    <> 
-    <main>
-      <h1>Latest Posts</h1>
-      {posts.map((post) => (<Post key={post.id} post={post}/>))}
-    </main>
-      
+    <>
+      <main>
+        <h1>Latest Posts</h1>
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </main>
     </>
-  )
+  );
 }
