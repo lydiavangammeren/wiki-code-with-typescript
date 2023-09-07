921675e81562606c99b18e44a14ff7d524dabc09
Lydia <67099359+lydiavangammeren@users.noreply.github.com>
2023-09-07 16:31:14 +0200

    added GET to api for [postId]

--- app/api/posts/[postId]/route.ts
+++ app/api/posts/[postId]/route.ts
@@ -1,10 +1,25 @@
 import prisma from "@/lib/prisma";
-
+import { NextResponse } from "next/server";
 interface Params {
     params: {
         postId: string;
     }
 }
+
+// GET api/posts/:postId
+export async function GET(request: Request, {params} : Params) {
+    const postId = Number(params.postId);
+    const post = await prisma.post.findUnique({
+      where: {
+        id: postId,
+      },
+      include: {
+        author: true,
+      },
+    });
+    return NextResponse.json(post);
+  }
+
 // Publish Post
 // PUT api/posts/:postId
 export async function PUT(request: Request, {params} : Params) {
